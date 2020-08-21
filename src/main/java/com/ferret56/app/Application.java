996b30ba42ed2f68692a8f2d1f788915235f10cf
Ferret56 <Ferret56@yandex.ru>
2020-08-21 19:50:02 +0300

    Initial commit

---
+++ src/main/java/com/ferret56/app/Application.java
@@ -0,0 +1,13 @@
+package com.ferret56.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class);
+  }
+
+}
