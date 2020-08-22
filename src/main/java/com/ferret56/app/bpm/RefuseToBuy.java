7c423782767759deab277abbe5ec31998c2b46b9
Ferret56 <Ferret56@yandex.ru>
2020-08-22 18:23:13 +0300

    logging

--- src/main/java/com/ferret56/app/bpm/RefuseToBuy.java
+++ src/main/java/com/ferret56/app/bpm/RefuseToBuy.java
@@ -1,13 +1,15 @@
 package com.ferret56.app.bpm;
 
+import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class RefuseToBuy implements JavaDelegate {
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
-        System.out.println("Oh, sorry:(");
+       log.info("Oh, sorry:(");
     }
 }
